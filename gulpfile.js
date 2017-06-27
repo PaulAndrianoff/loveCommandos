872e9d1e79a52f0fa8b9e569ceac3e09f4b2f880
Paul-maire <paul.maire@hetic.net>
2017-06-27 13:51:49 +0200

    Arch init
    Init file for the project

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const sass = require('gulp-sass')
+
+const config = {
+	js: {
+		src: ['src/js/**/*.js', 'src/js/*.js'],
+		dist: 'dist/js'
+	},
+	sass: {
+		src: ['src/sass/**/*.scss', 'src/sass/*.scss'],
+		dist: 'dist/css'
+	}
+}
+
+gulp.task('js', () => {
+	return gulp.src(config.js.src)
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(gulp.dest(config.js.dist))
+})
+
+gulp.task('sass', () => {
+	return gulp.src(config.sass.src)
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(gulp.dest(config.sass.dist))
+})
+
+gulp.task('sass:watch', () => {
+	gulp.watch(config.sass.src, ['sass']);
+})
+
+gulp.task('js:watch', () => {
+	gulp.watch(config.js.src, ['js']);
+})
+
+gulp.task('default', ['js', 'sass', 'js:watch', 'sass:watch'])
